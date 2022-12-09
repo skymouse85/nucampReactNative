0e94c6f91807853af52044525ebd155f4539a020
Mark Clifford <markmclifford@gmail.com>
2022-12-09 12:01:50 -0800

    Components part 2

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import { CAMPSITES } from "../shared/campsites";
-import DirectoryScreen from './DirectoryScreen';
 import { View } from 'react-native';
+import { CAMPSITES } from "../shared/campsites";
 import CampsiteInfoScreen from './CampsiteInfoScreen';
+import DirectoryScreen from './DirectoryScreen';
 
 
 const Main = () => {
     const [campsites, setCampsites] = useState(CAMPSITES);
-
+    const [selectedCampsiteId, setSelectedCampsiteId] = useState();
     return (
         <View style={{ flex: 1 }}>
             <DirectoryScreen
