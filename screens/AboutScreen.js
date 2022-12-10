4d2a0503ab9c91276cd7aaed61f8fc6769bf467c
Mark Clifford <markmclifford@gmail.com>
2022-12-10 13:30:15 -0800

    working task 2 need task 3

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -1,4 +1,5 @@
-import { ScrollView } from "react-native-gesture-handler"
+import { ScrollView, Text } from "react-native"
+import { Card } from "react-native-elements";
 
 const AboutScreen = () => {
     return (
