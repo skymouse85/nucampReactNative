b7b050c8fec64dd0eee3d2f648be3d3c58ecb3bf
Mark Clifford <markmclifford@gmail.com>
2022-12-14 14:47:42 -0800

    prior bug fixed

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import Main from "./screens/MainComponent";
 import { NavigationContainer } from "@react-navigation/native";
+import { Provider } from "react-redux";
+import { store } from './redux/store';
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Main />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Provider store={store}>
+          <Main />
+        </Provider>
+      </NavigationContainer>
+    </SafeAreaProvider>
   )
 }
 
